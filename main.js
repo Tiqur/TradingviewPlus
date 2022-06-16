e6bf224690a3a8de76d702832298560b77396dde
Tiqur <Tiqur7@gmail.com>
2022-06-15 17:46:27 -0700

    Various bugfixes

--- main.js
+++ main.js
@@ -224,14 +224,11 @@ const init = async () => {
 
 
     autoTimeframeElement.addEventListener('click', () => {
-      autoTimeframeElementButton.className = `button-G7o5fBfa ${autoTimeframeElementButton.className.includes('isActive') ? '' : 'isActive-G7o5fBfa'} autoTimeframeElementButton`
+      autoTimeframeElementButton.className = `button-G7o5fBfa ${autoTimeframeElementButton.className.includes('isActive') ? '' : 'isActive-G7o5fBfa'} autoTimeframeButton`
     })
 
     autoTimeframeElementArrow.addEventListener('click', () => {
-      button.className = `dropdown-m5d9X7vB  ${button.className.includes('isOpened') ? '' : 'isOpened-m5d9X7vB'} autoTimeframe`
-    })
-
-    autoTimeframeElementArrow.addEventListener('click', () => {
+      button.className = `dropdown-m5d9X7vB  ${button.className.includes('isOpened') ? '' : 'isOpened-m5d9X7vB'} autoTimeframe`;
       if (button.className.includes('isOpened')) {
         renderConfigMenu();
         document.getElementById('overlap-manager-root').appendChild(wrapper);
@@ -240,30 +237,30 @@ const init = async () => {
       }
  
     })
-
-    // Create new element
-    //let newElement = document.createElement('div');
-    //newElement.className = `button-G7o5fBfa dropdown-m5d9X7vB ${enabledOnStartup ? 'isActive-G7o5fBfa ' : ''} autoTimeframe`;
-    //newElement.innerHTML = '<div class="bg-G7o5fBfa"><span class="icon-G7o5fBfa"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28" width="28" height="28"><g fill="currentColor" fill-rule="evenodd"><path transform="translate(2, 2)" fill-rule="nonzero" d="m19 9 1.25-2.75L23 5l-2.75-1.25L19 1l-1.25 2.75L15 5l2.75 1.25L19 9zm-7.5.5L9 4 6.5 9.5 1 12l5.5 2.5L9 20l2.5-5.5L17 12l-5.5-2.5zM19 15l-1.25 2.75L15 19l2.75 1.25L19 23l1.25-2.75L23 19l-2.75-1.25L19 15z"></path></g></svg></span></div>';
-    //toolWrapper.insertAdjacentElement('beforebegin', newElement);
-    //
-    // Add event listener
   }
 
 
   document.getElementsByClassName("chart-gui-wrapper")[0].children[1].addEventListener('mousedown', () => {
     let currentTimeframe = [].slice.call(document.getElementById("header-toolbar-intervals").children).filter(e => e.className.includes("isActive"))[0].innerText;
+    const currentConfig = timeframeConfig.get(currentTimeframe);
+
+    // Close menu
+    if (document.getElementsByClassName('autoTimeframe')[0].className.includes('isOpened')) {
+      document.getElementsByClassName('autoTimeframeArrow')[0].click()
+    }
+
     const drawingToolsActive = document.getElementsByClassName("floating-toolbar-react-widgets__button button-khcLBZEz apply-common-tooltip newStyles-khcLBZEz")[1].title == "Line tool colors";
+
     
     // If drawing tools is open ( opens after line click ) and autoTimeframe is enabled
-    if (drawingToolsActive && document.getElementsByClassName('autoTimeframe')[0].className.includes('isActive')) {
+    if (drawingToolsActive && document.getElementsByClassName('autoTimeframeButton')[0].className.includes('isActive')) {
       // Click line tool color picker
       document.getElementsByClassName("floating-toolbar-react-widgets__button button-khcLBZEz apply-common-tooltip newStyles-khcLBZEz")[1].click()
-      //[].slice.call(document.getElementsByClassName("swatches-qgksmXjR")[2].children).filter(e => e.className.includes("tooltip"))[0].click();
 
-      //waitForElm("customButton-WiTVOllB apply-common-tooltip").then(document.getElementsByClassName("customButton-WiTVOllB apply-common-tooltip")[0].focus());
-      // Click color according to timeframe 
-      //document.getElementsByClassName("swatches-qgksmXjR")[2].children[timeframeConfig[currentTimeframe]].click()
+      // Set values
+      const allColorElements = [...[].slice.call(document.getElementsByClassName('menuBox-8MKeZifP')[0].children[0].children[0].children), ...[].slice.call(document.getElementsByClassName('menuBox-8MKeZifP')[0].children[0].children[1].children), ...[].slice.call(document.getElementsByClassName('menuBox-8MKeZifP')[0].children[0].children[3].children).filter(e => !e.getAttribute('title'))];
+      allColorElements.filter(e => e.style.color == currentConfig.color)[0].click();
+      //[].slice.call(document.getElementsByClassName('wrap-sYKPueSl')[0].children)[currentConfig.thickness-1].children[0].click()
     }
 
   });
