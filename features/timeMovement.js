017759325ef668abc0cb9b124229c3f187ee70a7
Tiqur <Tiqur7@gmail.com>
2022-08-30 09:02:52 -0700

    Fix undo hotkey triggering custom hotkey

--- features/timeMovement.js
+++ features/timeMovement.js
@@ -1,7 +1,7 @@
 function enableTimeMovementHotkeys(key1, key2) {
   // Handle both types of key presses
   function keyHandler(e) {
-    if (e.key === key1 || e.key === key2) 
+    if ((e.key === key1 || e.key === key2) && !e.ctrlKey)
       document.getElementsByClassName('chart-page')[0].dispatchEvent(new KeyboardEvent(e.type, {'keyCode': e.key === key1 ? 37 : 39, 'altKey': true, 'bubbles': true}));
   }
   
