abc74d82a717aa80da027b1233925eb4afd0d5ae
Tiqur <Tiqur7@gmail.com>
2022-09-05 14:24:27 -0700

    Deselect levels when chaning timeframes

--- features/autoTimeframeColors.js
+++ features/autoTimeframeColors.js
@@ -14,13 +14,13 @@ function enableAutoTimeframeColors() {
       // Get current timeframe
       const currentTimeframe = [].slice.call(document.querySelector('[id="header-toolbar-intervals"]').children).filter(e => e.className.includes('isActive'))[0].innerText;
 
+
       // Wait for toolbar
       waitForElm('.floating-toolbar-react-widgets__button').then((e) => {
         // Click Line tool colors on toolbar
         document.querySelector('[data-name="line-tool-color"]').click()
         const colorBox = document.querySelector('[data-name="menu-inner"]').children[0];
         const allColors = [...[].slice.call(colorBox.children[0].children), ...[].slice.call(colorBox.children[1].children)];
-        console.log(currentTimeframe)
         allColors[timeframeConfig.get(currentTimeframe)].click();
       })
     });
