f040d37c0183c8644399782b93d10e7cd8049301
Tiqur <Tiqur7@gmail.com>
2022-10-27 04:51:23 -0700

    Updated line style and line width

--- features/lineStyle.js
+++ features/lineStyle.js
@@ -5,7 +5,7 @@ function enableLineStyleHotkey(key) {
       document.querySelector('[data-name="style"]').click()
 
       // Line style scrolling
-      const styleButtons = [].slice.call(document.querySelector('[data-name="menu-inner"]').children[0].children[0].children).filter(e => e.children.length > 1);
+      const styleButtons = [].slice.call(document.querySelectorAll('[data-name="menu-inner"]')[1].children[0].children[0].children).filter(e => e.children.length > 1);
       var activeIndex = styleButtons.findIndex(e => e.className.includes(' active-'))
       styleButtons[activeIndex != 2 ? activeIndex+1 : 0].click();
     }
--- features/lineWidth.js
+++ features/lineWidth.js
@@ -4,7 +4,7 @@ function enableLineWidthHotkey(key) {
       // Click line thickness button
       document.querySelector('[data-name="line-tool-width"]').click()
       // Line thickness scrolling
-      const thicknessButtons = [].slice.call(document.querySelector('[data-name="menu-inner"]').children);
+      const thicknessButtons = [].slice.call(document.querySelectorAll('[data-name="menu-inner"]')[1].children);
       var activeIndex = thicknessButtons.findIndex(e => e.className.includes('isActive'))
       thicknessButtons[activeIndex != 3 ? activeIndex+1 : 0].click();
     }
