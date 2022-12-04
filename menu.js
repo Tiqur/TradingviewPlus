d86987d2e621d110b39398b6e6b7f40a2a57ffa3
Tiqur <Tiqur7@gmail.com>
2022-12-04 13:46:27 -0800

    Added tooltips

--- menu.js
+++ menu.js
@@ -6,6 +6,25 @@ let hotkey = null;
 let hotkeyCache = null;
 let featureCache = null;
 
+const questionMarkSvgString = `
+<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 fill='white' width="12px" height="12px" viewBox="0 0 40.124 40.124" style="enable-background:new 0 0 40.124 40.124;"
+	 xml:space="preserve">
+  <g>
+    <g>
+      <path d="M19.938,12.141c1.856,0,2.971,0.99,2.971,2.66c0,3.033-5.414,3.869-5.414,7.55c0,0.99,0.648,2.072,1.979,2.072
+        c2.042,0,1.795-1.516,2.538-2.6c0.989-1.453,5.6-3,5.6-7.023c0-4.361-3.897-6.188-7.858-6.188c-3.773,0-7.24,2.692-7.24,5.725
+        c0,1.237,0.929,1.887,2.012,1.887C17.525,16.225,15.979,12.141,19.938,12.141z"/>
+      <path d="M22.135,28.973c0-1.393-1.145-2.537-2.537-2.537s-2.537,1.146-2.537,2.537c0,1.393,1.145,2.537,2.537,2.537
+        S22.135,30.366,22.135,28.973z"/>
+      <path d="M40.124,20.062C40.124,9,31.124,0,20.062,0S0,9,0,20.062s9,20.062,20.062,20.062S40.124,31.125,40.124,20.062z M2,20.062
+        C2,10.103,10.103,2,20.062,2c9.959,0,18.062,8.103,18.062,18.062c0,9.959-8.103,18.062-18.062,18.062
+        C10.103,38.124,2,30.021,2,20.062z"/>
+    </g>
+  </g>
+</svg>`;
+
+
 let menu = {
   'Features': {
     'Auto Timeframe Colors': {
@@ -178,6 +197,18 @@ title.style.color = 'white';
 contentTitle.appendChild(title);
 margin.appendChild(contentTitle);
 
+const tooltipContainer = document.createElement('div');
+tooltipContainer.style.background = '#434651';
+tooltipContainer.style.border = '0.5px solid #787b86'
+tooltipContainer.style.padding = '0.8em';
+tooltipContainer.style.borderRadius = '6px';
+tooltipContainer.style.position = 'absolute';
+tooltipContainer.style.display = 'flex';
+tooltipContainer.style.justifyContent = 'center';
+tooltipContainer.style.alignItems = 'center';
+tooltipContainer.style.width = '200px';
+tooltipContainer.style.wordWrap = 'break-word';
+
 const contentBody = document.createElement('div');
 contentBody.style.background = '#151924';
 contentBody.style.width = '80%';
@@ -203,6 +234,8 @@ Object.keys(menu).forEach((e, i) => {
   containerTitle.style.fontSize = '20px';
   containerTitle.style.marginLeft = '1em';
   container.appendChild(containerTitle);
+
+
 
 
   const dropdownContent = document.createElement('div');
@@ -290,7 +323,23 @@ Object.keys(menu).forEach((e, i) => {
 
         featureCheckBoxDiv.appendChild(hotkeyDiv);
         const featureTitle = document.createElement('p');
-        featureTitle.innerHTML = feature;
+        featureTitle.innerHTML = feature + questionMarkSvgString;
+
+        // Tooltip logic
+        featureTitle.querySelector('svg').addEventListener('mouseover', e => {
+          tooltipContainer.style.display = 'flex';
+          tooltipContainer.innerHTML = menu['Features'][feature].tooltip;
+          tooltipContainer.style.left = e.clientX + 'px';
+          tooltipContainer.style.top = e.clientY - tooltipContainer.getBoundingClientRect().height + 'px';
+          
+        })
+        featureTitle.querySelector('svg').addEventListener('mouseout', () => {
+          tooltipContainer.style.left = '0';
+          tooltipContainer.style.top = '0';
+          tooltipContainer.style.display = 'none';
+          tooltipContainer.innerHTML = '';
+        })
+
         priceCheckboxContainer.appendChild(featureTitle);
         featureCheckBoxDiv.style.display = 'flex';
         featureCheckBoxDiv.style.justifyContent = 'space-between';
@@ -316,8 +365,10 @@ Object.keys(menu).forEach((e, i) => {
   contentBody.appendChild(dropdownContent);
 });
 
+
 waitForElm('#overlap-manager-root').then(() => {
   document.getElementById('overlap-manager-root').appendChild(container);
+  document.getElementById('overlap-manager-root').appendChild(tooltipContainer)
 })
 
 function setHotkey(feature, keys) {
