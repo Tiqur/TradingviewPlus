6dc23a6c34de194e385fa446ad73788fda7f7629
Tiqur <trevorbrage02@gmail.com>
2023-06-17 13:02:15 -0700

    Add checkboxes and tooltip svg to features

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,24 @@ function waitForElm(selector: string): Promise<Element | null> {
     });
 }
 
+const questionMarkSvgString = `
+<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 fill='white' width="12px" height="12px" viewBox="0 0 40.124 40.124" style="enable-background:new 0 0 40.124 40.124;"
+	 xml:space="preserve">
+  <g>
+    <g>
+      <path d="M19.938,12.141c1.856,0,2.971,0.99,2.971,2.66c0,3.033-5.414,3.869-5.414,7.55c0,0.99,0.648,2.072,1.979,2.072
+        c2.042,0,1.795-1.516,2.538-2.6c0.989-1.453,5.6-3,5.6-7.023c0-4.361-3.897-6.188-7.858-6.188c-3.773,0-7.24,2.692-7.24,5.725
+        c0,1.237,0.929,1.887,2.012,1.887C17.525,16.225,15.979,12.141,19.938,12.141z"/>
+      <path d="M22.135,28.973c0-1.393-1.145-2.537-2.537-2.537s-2.537,1.146-2.537,2.537c0,1.393,1.145,2.537,2.537,2.537
+        S22.135,30.366,22.135,28.973z"/>
+      <path d="M40.124,20.062C40.124,9,31.124,0,20.062,0S0,9,0,20.062s9,20.062,20.062,20.062S40.124,31.125,40.124,20.062z M2,20.062
+        C2,10.103,10.103,2,20.062,2c9.959,0,18.062,8.103,18.062,18.062c0,9.959-8.103,18.062-18.062,18.062
+        C10.103,38.124,2,30.021,2,20.062z"/>
+    </g>
+  </g>
+</svg>`;
+
 // Disable default TV hotkeys
 document.addEventListener("keypress", (event) => event.stopPropagation(), true);
 
@@ -79,10 +97,18 @@ function initMenuResizeLogic() {
     for (const key in menu_contents) {
       const value = menu_contents[key];
       const parent = document.querySelector(`[id="tvp-${value.category}"]`)
+
+      const content_container = document.createElement('div');
+
+      const input = document.createElement('input');
+      input.type = 'checkbox';
+      content_container.appendChild(input);
+
       const node = document.createElement('p');
-      node.innerText = value.name;
-      console.log(parent);
-      parent?.appendChild(node);
+      node.innerHTML = value.name + questionMarkSvgString;
+      content_container.appendChild(node);
+
+      parent?.appendChild(content_container);
     }
   });
 })();
