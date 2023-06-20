8a4c1815125860b470c2c42fafa53b0564357854
Tiqur <trevorbrage02@gmail.com>
2023-06-19 19:45:15 -0700

    Added Toggle TVP Menu feature

---
+++ src/features/toggleMenu.ts
@@ -0,0 +1,23 @@
+class ToggleMenu extends FeatureClass {
+  init() {
+    document.addEventListener('keydown', e => {
+      if (this.checkTrigger(e) && this.isEnabled())
+        (document.getElementById('tvp-menu-button')?.children[0] as HTMLElement).click();
+    });
+  }
+}
+
+features['Toggle TVP Menu'] = new ToggleMenu({
+  name: 'Toggle TVP Menu',
+  tooltip: 'Opens and closes the TVP menu',
+  enabled: true,
+  hotkey: {
+    key: 'a',
+    alt: false,
+    shift: false,
+    ctrl: false,
+    meta: false
+  },
+  category: 'Features',
+  action: () => {}
+});
