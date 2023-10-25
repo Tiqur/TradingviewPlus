a9f368a195a3a459c8e0f33f6a8d8b7eab0a80a7
Tiqur <trevorbrage02@gmail.com>
2023-10-24 18:09:12 -0700

    add basic methods to storage service

--- src/di/services/StorageService.ts
+++ src/di/services/StorageService.ts
@@ -1,3 +1,28 @@
 export default class StorageService {
+  storage: Map<string, any> = new Map();
+
+  constructor(key: string) {
+    (async () => {
+
+      const storageObj: Record<string, any> = await this.fetchStorage(key);
+
+      // Use locally stored data if available
+      if (!this.storageIsEmpty(storageObj)) {
+       this.setStorage(new Map(Object.entries(JSON.parse(storageObj[key]))));
+      }
+    })
+  }
+
+  setStorage(newConfig: Map<string, any>) {
+
+  }
+
+  async fetchStorage(key: string): Promise<object> {
+    return await browser.storage.local.get(key);
+  }
+
+  storageIsEmpty(storageObj: object) {
+    return Object.keys(storageObj).length === 0;
+  }
 
 }
