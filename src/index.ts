f7e26900b88bd79c147bed957c0db3a978471b66
Tiqur <trevorbrage02@gmail.com>
2023-10-26 16:19:00 -0700

    rudimentary dynamic dropdown content injection + styles

--- src/index.ts
+++ src/index.ts
@@ -31,5 +31,28 @@ function runAfterInjection() {
         container.style.width = menuContainerWidth+'px';
       }
     }
-  })
+  });
+
+
+  // Dynamically insert features into menu
+  const featuresArr = Array.from(features.values());
+
+  featuresArr.forEach(feature => {
+    const category = feature.getCategory();
+    const p = document.createElement('p');
+    p.innerText = feature.getName();
+
+    let categoryContainer;
+    switch(category) {
+      case Category.TV:
+        categoryContainer = document.getElementById('tradingview-dropdown-content-container');
+        break;
+      case Category.TVP:
+        categoryContainer = document.getElementById('custom-dropdown-content-container');
+        break;
+    }
+
+
+    categoryContainer?.appendChild(p);
+  });
 }
