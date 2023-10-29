5791465fb617c1fc2d6a88d765e63cf897379644
Tiqur <trevorbrage02@gmail.com>
2023-10-29 15:12:17 -0700

    Create build script + update .gitignore

---
+++ build.sh
@@ -0,0 +1,18 @@
+# Compile typescript
+tsc
+
+# Compile scss
+npx sass public/:dist/
+
+# Create temp dir
+mkdir temp
+
+# Copy files into temp dir
+cp -r manifest.json dist/* temp
+
+# Zip contents "extension.xpi"
+zip extension.xpi temp
+
+# Delete temp dir
+rm -r temp
+
