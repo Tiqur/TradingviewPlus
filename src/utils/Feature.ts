b83809f0eabde60d1922b861b1527831b39f8ea7
Tiqur <trevorbrage02@gmail.com>
2023-11-05 09:36:25 -0800

    Remove unneeded logs

--- src/utils/Feature.ts
+++ src/utils/Feature.ts
@@ -60,7 +60,6 @@ abstract class Feature {
 
         const keyupListener = () => {
           // Update 'this.hotkey' with the newly selected hotkey
-          console.log("new hotkey:", hotkey);
           this.setHotkey(hotkey)
 
           // Re-render menu while maintaining fuzzy search results
@@ -110,8 +109,6 @@ abstract class Feature {
       this.saveToLocalStorage();
     }
 
-    console.log(this.config);
-    //console.log(this.getConfigValue('once'));
   }
 
 
