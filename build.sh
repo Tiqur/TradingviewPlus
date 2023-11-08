545ad682c47fa2a90baaae53b9c6ead589acb7fd
Tiqur <trevorbrage02@gmail.com>
2023-11-08 14:28:20 -0800

    Use seperate path for platform manifests

--- build.sh
+++ build.sh
@@ -2,24 +2,28 @@
 
 # Check if at least one command line argument is provided
 if [ $# -lt 1 ]; then
-  echo "Usage: $0 [firefox|chrome]"
+  echo "Usage: $0 [firefox|chrome|opera]"
   exit 1
 fi
 
-MANIFEST_NAME=""
+MANIFEST_PATH=""
 OUTPUT_FILE_NAME=""
 
 # Check the value of the first command line argument
 if [ "$1" == "firefox" ]; then
   echo "Building for Firefox..."
-  MANIFEST_NAME="firefox-manifest.json"
+  MANIFEST_PATH="platform/firefox/manifest.json"
   OUTPUT_FILE_NAME="extension.xpi"
 elif [ "$1" == "chrome" ]; then
   echo "Building for Chrome..."
-  MANIFEST_NAME="chrome-manifest.json"
+  MANIFEST_PATH="platform/chrome/manifest.json"
+  OUTPUT_FILE_NAME="extension.zip"
+elif [ "$1" == "opera" ]; then
+  echo "Building for Opera..."
+  MANIFEST_PATH="platform/opera/manifest.json"
   OUTPUT_FILE_NAME="extension.zip"
 else
-  echo "Invalid argument. Supported values are 'firefox' and 'chrome'."
+  echo "Invalid argument. Supported values are 'firefox', 'chrome' and 'opera'."
   exit 1
 fi
 
@@ -35,11 +39,11 @@ mkdir temp
 # Copy files into temp dir
 cp -r $MANIFEST_NAME public/ dist/ temp
 
+# Copy manifest to temp
+cp $MANIFEST_PATH temp/
+
 # Go into temp dir
 cd temp
-
-# Rename manifest
-mv *manifest.json manifest.json
 
 # Zip contents "extension.xpi"
 zip -x "*.map" -x "*.scss" -r $OUTPUT_FILE_NAME *
