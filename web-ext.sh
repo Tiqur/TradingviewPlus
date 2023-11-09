ba18e35fd860fcf0e251c2043fe58c0413ee8512
Tiqur <trevorbrage02@gmail.com>
2023-11-09 11:08:28 -0800

    Install node modules + compile sass and ts before web-ext

--- web-ext.sh
+++ web-ext.sh
@@ -1,2 +1,14 @@
+# Install node libraries
+npm install
+
+# Compile typescript
+tsc
+
+# Compile scss
+npx sass public/:dist/
+
+# Copy firefox manifest to root dir
 cp platform/firefox/manifest.json .
+
+# Run web-ext
 TMPDIR=~/tmp-dir/ web-ext run
