61fd643b70780990cdb8cedbc43ff603ee42f09f
Tiqur <trevorbrage02@gmail.com>
2024-05-27 14:15:52 -0700

    Added default URL when using web-ext

--- web-ext.sh
+++ web-ext.sh
@@ -21,20 +21,22 @@ cp -r lib/ dist/
 # Remove old manifest
 rm manifest.json
 
+WEB_URL="https://www.tradingview.com/chart/?symbol=BITSTAMP%3ABTCUSD"
+
 # Copy firefox manifest to root dir then run web-ext
 # Check the value of the first command line argument
 if [ "$1" == "firefox" ]; then
   echo "Starting web-ext for Firefox..."
   cp platform/firefox/manifest.json .
-  TMPDIR=~/tmp-dir/ npx web-ext run
+  TMPDIR=~/tmp-dir/ npx web-ext run -u $WEB_URL
 elif [ "$1" == "chrome" ]; then
   echo "Starting web-ext for Chrome..."
   cp platform/chrome/manifest.json .
-  TMPDIR=~/tmp-dir/ npx web-ext run -t chromium
+  TMPDIR=~/tmp-dir/ npx web-ext run -t chromium -u $WEB_URL
 elif [ "$1" == "opera" ]; then
   echo "Starting web-ext for Opera..."
   cp platform/opera/manifest.json .
-  TMPDIR=~/tmp-dir/ npx web-ext run -t chromium --chromium-binary /usr/bin/opera
+  TMPDIR=~/tmp-dir/ npx web-ext run -t chromium --chromium-binary /usr/bin/opera -u $WEB_URL
 else
   echo "Invalid argument. Supported values are 'firefox', 'chrome' and 'opera'."
   exit 1
